0fe6be407871a9c72fdfc67bff218f20553789fa
Natt-QA <larrondo.nat@gmail.com>
2026-05-21 11:57:48 -0300

    Clase main con flujo de Login y Registro

--- src/projava/Usuario.java
+++ src/projava/Usuario.java
@@ -1,10 +1,11 @@
-package java;
+package projava;
 
 public class Usuario {
 
     private String nombre;
     private String apellido;
     private String email;
+    private String pais;
     private String contrasena;
 
 }
